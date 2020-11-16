069769bc333480cbfee19c02847c29752c329253
Miroslav Slusarčík <miroslav@slusarcik.cz>
2020-11-16 23:29:20 +0100

    Setup uglify for JS

--- GruntFile.js
+++ GruntFile.js
@@ -21,11 +21,27 @@ module.exports = function(grunt) {
           ext: '.min.css'
         }]
       }
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'public/assets/js/min/bootstrap.min.js': ['node_modules/bootstrap/dist/js/bootstrap.js'],
+          'public/assets/js/min/main.min.js': ['public/assets/js/main.js']
+        }
+      }
     }
   });
 
+  //SASS to CSS
   grunt.loadNpmTasks('grunt-contrib-sass');
+  //Minify CSS
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  //Minify JS
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['sass', 'cssmin']);
+  //Set default task
+  grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
 };
